125af4082464a87f291a6e5449e8d58020d322b4
Jayceon <jayceon310893@gmail.com>
2023-02-19 20:59:50 +0300

    create website and Header Qwilix

---
+++ src/components/HeaderQwilix.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="header-block">
+    <div :class= "!drawer? 'hamburger hamburger--arrow-r' : 'hamburger is-active hamburger--arrow-r'" @click="drawer = !drawer">
+      <div class="hamburger-box">
+        <div class="hamburger-inner" >
+        </div>
+      </div>
+    </div>
+  </div>
+  <q-drawer
+    v-model="drawer"
+    show-if-above
+    :width="200"
+    :breakpoint="400"
+  >
+    <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+      <q-list padding>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="inbox" />
+          </q-item-section>
+
+          <q-item-section>
+            Inbox
+          </q-item-section>
+        </q-item>
+
+        <q-item active clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="star" />
+          </q-item-section>
+
+          <q-item-section>
+            Star
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="send" />
+          </q-item-section>
+
+          <q-item-section>
+            Send
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="drafts" />
+          </q-item-section>
+
+          <q-item-section>
+            Drafts
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-scroll-area>
+
+    <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+      <div class="absolute-bottom bg-transparent">
+        <q-avatar size="56px" class="q-mb-sm">
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+        </q-avatar>
+        <div class="text-weight-bold">Razvan Stoenescu</div>
+        <div>@rstoenescu</div>
+      </div>
+    </q-img>
+  </q-drawer>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: "HeaderQwilix",
+  setup () {
+    return {
+      drawer: ref(false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header-block{
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  height: 50px;
+}
+@media screen and (min-width: 700px) {
+  .header-block{
+    width: 70px;
+    background: lightgray;
+    display: flex;
+    height: 100vh;
+    float: right;
+    align-items: center;
+  }
+  .hamburger{
+    transform: rotate(-90deg);
+  }
+}
+.hamburger {
+  z-index: 9999;
+  position: fixed;
+  padding: 15px 15px;
+  display: inline-block;
+  cursor: pointer;
+  transition-property: opacity, filter;
+  transition-duration: 0.15s;
+  transition-timing-function: linear;
+  font: inherit;
+  color: inherit;
+  text-transform: none;
+  background-color: transparent;
+  border: 0;
+  margin: 0;
+  overflow: visible; }
+.hamburger:hover {
+  opacity: 0.7; }
+.hamburger.is-active:hover {
+  opacity: 0.7; }
+.hamburger.is-active .hamburger-inner,
+.hamburger.is-active .hamburger-inner::before,
+.hamburger.is-active .hamburger-inner::after {
+  background-color: #000; }
+
+.hamburger-box {
+  width: 40px;
+  height: 24px;
+  display: inline-block;
+  position: relative; }
+
+.hamburger-inner {
+  display: block;
+  top: 50%;
+  margin-top: -2px; }
+.hamburger-inner, .hamburger-inner::before, .hamburger-inner::after {
+  width: 40px;
+  height: 4px;
+  background-color: #000;
+  border-radius: 4px;
+  position: absolute;
+  transition-property: transform;
+  transition-duration: 0.15s;
+  transition-timing-function: ease; }
+.hamburger-inner::before, .hamburger-inner::after {
+  content: "";
+  display: block; }
+.hamburger-inner::before {
+  top: -10px; }
+.hamburger-inner::after {
+  bottom: -10px; }
+.hamburger--arrow-r.is-active .hamburger-inner::before {
+  transform: translate3d(8px, 0, 0) rotate(45deg) scale(0.7, 1); }
+
+.hamburger--arrow-r.is-active .hamburger-inner::after {
+  transform: translate3d(8px, 0, 0) rotate(-45deg) scale(0.7, 1); }
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <header-qwilix></header-qwilix>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import HeaderQwilix from "components/HeaderQwilix";
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    HeaderQwilix,
+  },
+
+})
+</script>
