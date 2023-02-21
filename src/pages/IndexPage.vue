f00debd74c5de13a5217443675df63b88639ec00
Jayceon <jayceon310893@gmail.com>
2023-02-21 22:28:29 +0300

    second block

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -10,7 +10,27 @@
         <h5>Welcome to our company, a leading provider of website development services. Our team of skilled professionals is dedicated to creating stunning websites that not only look great, but also provide exceptional user experiences.</h5>
       </div>
     </div>
-    <div class="second-block"></div>
+    <div class="second-block">
+      <div class="second-block-text">
+        <div class="second-block-p">
+          <p>We are two professional web developers who specialize in Vue.js. Our expertise in this cutting-edge technology allows us to create dynamic and responsive web applications that provide an excellent user experience. Our commitment to quality and attention to detail ensures that the end product is both functional and visually appealing. As Vue.js continues to evolve, we remain dedicated to staying up-to-date with the latest developments and incorporating them into our work.</p>
+        </div>
+      </div>
+      <div class="block-developers">
+      <div class="block-vit">
+        <img src="/img/vit.png">
+        <p>Vitalii Alferuk</p>
+        <p>Nickname: Jayceon</p>
+        <p>FullStack Developer</p>
+      </div>
+        <div class="block-dyuk">
+          <img src="/img/dyuk.png">
+          <p>Andrey Alferuk</p>
+          <p>Nickname: Dyuk</p>
+          <p>FullStack Developer</p>
+        </div>
+      </div>
+    </div>
     <div class="third-block"></div>
   </q-page>
 </template>
@@ -81,17 +101,28 @@ export default {
 .second-block{
   width: 100vw;
   height: 100vh;
-  background: blue;
 }
 .third-block{
   width: 100vw;
   height: 100vh;
-  background: green;
 }
 .first-block-text{
   color: white;
+  display: flex;
+  align-items: flex-end;
 }
-
+.second-block{
+  display: flex;
+}
+.second-block-text p{
+  padding: 50px;
+}
+.block-developers{
+  width: 60%;
+  display: flex;
+  justify-content: center;
+  flex-flow: column;
+}
 @media only screen and (min-width: 780px) {
   .q-page.horizontal-scroll {
     display: flex;
@@ -126,6 +157,39 @@ export default {
     display: flex;
     align-self: flex-end;
     padding-right: 130px;
+  }
+  .second-block-text{
+    width: 40%;
+    border-right: black 1px solid;
+  }
+  .second-block-p{
+    display: flex;
+    height: 100%;
+    align-items: flex-end;
+    font-weight: bold;
+  }
+  .block-vit{
+    height: 38%;
+    display: flex;
+    text-align: center;
+    margin: 40px;
+    flex-flow: column;
+    font-weight: bold;
+  }
+  .block-dyuk{
+    height: 38%;
+    display: flex;
+    text-align: center;
+    margin: 40px;
+    flex-flow: column;
+    font-weight: bold;
+  }
+  .block-developers img{
+    width: 30%;
+    align-self: center;
+  }
+  .second-block{
+    border-right: black 1px solid;
   }
 }
 </style>
